e33adb45653037613bd2a95a1cf9188bf41780b3
Lakshdeep Singh <lakshdeep.code@gmail.com>
2024-12-10 09:21:20 +0530

    without database test

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -7,16 +7,18 @@ export async function POST(req: Request) {
   try {
     const body = await req.json();
     const { username, email, password } = body;
+    console.log("Username: " + username + " Email: " + email);
 
-    const newUser = await prisma.user.create({
-      data: {
-        name: username,
-        email,
-        password,
-      },
-    });
+    // const newUser = await prisma.user.create({
+    //   data: {
+    //     username,
+    //     email,
+    //     password,
+    //   },
+    // });
+    // console.log(newUser)
 
-    return NextResponse.json({ success: true, user: newUser });
+    return NextResponse.json({ success: true});
   } catch (error) {
     console.error("Error creating user:", error);
     return NextResponse.json({ success: false, error: "Error creating user" }, { status: 500 });
